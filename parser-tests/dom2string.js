55f68db7159cdb36c00624f3a21dfd8d653f5fef
Anne van Kesteren <annevk@annevk.nl>
2007-07-05 12:00:01 +0000

    automated parser testing

---
+++ parser-tests/dom2string.js
@@ -0,0 +1,90 @@
+function indent(ancestors) {
+  var str = "";
+  if (ancestors > 0) {
+    while (ancestors--)
+      str += "  ";
+  }
+  return str;
+}
+
+function dom2string(node, ancestors) {
+  var str = "";
+  if (typeof ancestors == "undefined")
+    var ancestors = 0;
+  if (!node.firstChild)
+    return "\n| ";
+  var parent = node;
+  var current = node.firstChild;
+  var next = null;
+  var misnested = null;
+  for (;;) {
+    str += "\n| " + indent(ancestors);
+    switch (current.nodeType) {
+      case 10:
+        str += '<!DOCTYPE ' + current.nodeName + '>';
+        break;
+      case 8:
+        try {
+          str += '<!-- ' + current.nodeValue + ' -->';
+        } catch (e) {
+          str += '<!--  -->';
+        }
+        if (parent != current.parentNode) {
+          return str += ' (misnested... aborting)';
+        }
+        break;
+      case 7:
+        str += '<?' + current.nodeName + current.nodeValue + '>';
+        break;
+      case 4:
+        str += '<![CDATA[ ' + current.nodeValue + ' ]]>';
+        break;
+      case 3:
+        str += '"' + current.nodeValue + '"';
+        if (parent != current.parentNode) {
+          return str += ' (misnested... aborting)';
+        }
+        break;
+      case 1:
+        str += "<" + current.nodeName.toLowerCase() + '>'; // seems there's no way to get the name in a case-preserving manner :-(
+        if (parent != current.parentNode) {
+          return str += ' (misnested... aborting)';
+        } else {
+          if (current.attributes) {
+            var arrAttrs = [];
+            for (var j = 0; j < current.attributes.length; j += 1) {
+              if (current.attributes[j].specified) {
+                arrAttrs.push(current.attributes[j].nodeName);
+              }
+            }
+            if (arrAttrs.length > 0) {
+              arrAttrs.sort();
+              for (var j = 0; j < arrAttrs.length; j += 1) {
+                str += "\n| " + indent(1 + ancestors) + arrAttrs[j];
+                str += '="' + current.attributes[arrAttrs[j]].nodeValue + '"';
+              }
+            }
+          }
+          if (next = current.firstChild) {
+            parent = current;
+            current = next;
+            ancestors++;
+            continue;
+          }
+        }
+        break;
+    }
+    for (;;) {
+      if (next = current.nextSibling) {
+        current = next;
+        break;
+      }
+      current = current.parentNode;
+      parent = parent.parentNode;
+      ancestors--;
+      if (current == node) {
+        return str;
+      }
+    }
+  }
+}
