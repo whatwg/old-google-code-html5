e3e9fe2e96595ca2b120fd52e20724b7e99a0665
Simon Pieters <zcorpan@gmail.com>
2007-08-26 12:50:35 +0000

    lachy's changes (adds "big issue: " to issues)

--- status/annotate-web-apps.js
+++ status/annotate-web-apps.js
@@ -61,3 +61,89 @@ if(an3err.number == -0x7FFF0001) style.styleSheet.cssText =
 styleText.nodeValue 
 else throw an3err } 
 document.getElementsByTagName("head")[0].appendChild(style);
+
+/*
+ * Author: Lachlan Hunt - http://lachy.id.au/
+ * Date: 2007-08-15
+ *
+ * getElementsByClassName was wrtten by Tino Zijdel
+ * http://therealcrisp.xs4all.nl/blog/2007/08/13/getelementsbyclassname-re-re-re-visited/
+ */
+ 
+var getElementsByClassName = function()
+{
+    // native
+    if (document.getElementsByClassName)
+    {
+        return function(className, nodeName, parentElement)
+        {
+            var s = (parentElement || document).getElementsByClassName(className);
+
+            if (nodeName && nodeName != '*')
+            {
+                nodeName = nodeName.toUpperCase();
+                return Array.filter(s, function(el) { return el.nodeName == nodeName; });
+            }
+            else
+                return [].slice.call(s, 0);
+        }
+    }
+
+    // xpath
+    if (document.evaluate)
+    {
+        return  function(className, nodeName, parentElement)
+        {
+            if (!nodeName) nodeName = '*';
+            if (!parentElement) parentElement = document;
+
+            var results = [], s, i = 0, element;
+
+            s = document.evaluate(
+                ".//" + nodeName + "[contains(concat(' ', @class, ' '), ' " + className + " ')]",
+                parentElement,
+                null,
+                XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+                null
+            );
+
+            while ((element = s.snapshotItem(i++)))
+                results.push(element);
+
+            return results;
+        }
+    }
+
+    // generic
+    return function(className, nodeName, parentElement)
+    {
+        if (!nodeName) nodeName = '*';
+        if (!parentElement) parentElement = document;
+
+        var results = [], s, i = 0, element;
+        var re = new RegExp('(^|\s)' + className + '(\s|$)'), elementClassName;
+
+        s = parentElement.getElementsByTagName(nodeName);
+
+        while ((element = s[i++]))
+        {
+            if (    (elementClassName = element.className) &&
+                (elementClassName == className || re.test(elementClassName))
+            )
+                results.push(element);
+        }
+
+        return results;
+    }
+}();
+
+function markIssue(issue) {
+	var marker = document.createElement("strong");
+	marker.innerHTML = "Big Issue: ";
+	issue.insertBefore(marker, issue.firstChild);
+}
+
+var issues = getElementsByClassName("big-issue");
+for (var i = 0; i < issues.length; i++) {
+	markIssue(issues[i]);
+}
