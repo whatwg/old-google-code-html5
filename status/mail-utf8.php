25f92000bd74ac1c5732ec640fca98bd979dc950
Simon Pieters <zcorpan@gmail.com>
2007-08-27 16:22:20 +0000

    \n -> \r\n per rfc2646

--- status/mail-utf8.php
+++ status/mail-utf8.php
@@ -3,10 +3,10 @@
 function sendMail($toName, $toEmail, $body) {
 	$to       = "=?UTF-8?B?" . base64_encode($toName) . "?= <$toEmail>";
 	$subject  = "=?UTF-8?B?" . base64_encode("HTML5 Status Update") . "?=";
-	$sig      = "HTML5 Status Updates\nhttp://www.w3.org/html/\nhttp://www.whatwg.org/";
-	$message  = "$body\n-- \n$sig";
-	$headers  = "MIME-Version: 1.0\n";
-	$headers .= "Content-Type: text/plain; charset=utf-8; format=flowed\n";
+	$sig      = "HTML5 Status Updates\r\nhttp://www.w3.org/html/\r\nhttp://www.whatwg.org/";
+	$message  = "$body\r\n-- \r\n$sig";
+	$headers  = "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/plain; charset=utf-8; format=flowed\r\n";
 	$headers .= "From: =?UTF-8?B?" . base64_encode("HTML5 Status") . "?= <whatwg@whatwg.org>";
 	
 	mail($to, $subject, $message, $headers);
