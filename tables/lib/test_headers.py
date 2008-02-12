5c143a42b1c55693394107257796bf1d48d3908b
jgraham.cantab <jgraham@hoppipolla.co.uk>
2008-02-12 18:21:28 +0000

    A few more testcases

--- tables/lib/test_headers.py
+++ tables/lib/test_headers.py
@@ -7,22 +7,22 @@ import html5lib
 import tableparser
 import headers
 
-#Test harness for headers code
-#The input is json format in a file testdata/headers/*.test
-#
-#the file format is:
-# {"tests":
-#          {"input":"html input string",
-#            "cases":[
-#                     ["algorithm_name", {"algorithm_option":option_value}, 
-#                       {"cell textContent":[list of header text content]}
-#                     ],
-#                      more cases... 
-#                    ]
-#           },
-#           more tests ...
-# }
-#Each cell in input must have textContent that is unique in the table
+"""Test harness for headers code
+The input is json format in a file testdata/headers/*.test
+
+the file format is:
+ {"tests":
+          {"input":"html input string",
+            "cases":[
+                     ["algorithm_name", {"algorithm_option":option_value}, 
+                       {"cell textContent":[list of header text content]}
+                     ],
+                      more cases... 
+                    ]
+           },
+           more tests ...
+ }
+Each cell in input must have textContent that is unique in the table"""
 
 matchers = dict([(item, getattr(headers, item).HeadingMatcher) for item in 
                  ("html4", "html5", "experimental", "smartcolspan", 
@@ -30,10 +30,15 @@ matchers = dict([(item, getattr(headers, item).HeadingMatcher) for item in
 
 def childText(node, addTail=False):
     """Return the textContent of an lxml node"""
-    rv = node.text
+    if node.text:
+        rv = node.text
+    else:
+        rv = ""
     for child in node:
-        rv += childText(node, True)
-    if addTail:
+        child_text = childText(child, True)
+        if child_text is not None:
+            rv += child_text
+    if addTail and node.tail is not None:
         rv += node.tail
     return rv
 
@@ -45,6 +50,7 @@ def parseTable(document):
     return tparser.parse(table)
 
 def compareHeaders(text_cell_map, headers_map, expected_results):
+    """Actually compare the headers"""
     for cell_text, expected_headers_text in expected_results.iteritems():
         cell = text_cell_map[cell_text]
         expected_headers = set([text_cell_map[item] for item in expected_headers_text])
@@ -83,6 +89,7 @@ def runtest(testdata):
     
 
 def test_tableparser():
+    """Load all the tests"""
     for testfile in glob.glob("testdata/headers/*.test"):
         tests = simplejson.load(open(testfile))
         for test in tests["tests"]:
